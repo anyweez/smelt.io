1a709ea3ff4cd3f96136ef3b1684a45750af0dc2
Luke Segars <luke@theironyard.com>
2016-07-30 18:25:50 -0400

    Added example challenges.

---
+++ challenges/foxtrot.js
@@ -0,0 +1,4 @@
+import test from 'ava';
+let target = require('./_smelt.target');
+
+test('foxtrot test passes', t => t.pass());
---
+++ challenges/sum.js
@@ -0,0 +1,4 @@
+import test from 'ava';
+let target = require('./_smelt.target');
+
+test('sum test succeeds', t => test.pass());
