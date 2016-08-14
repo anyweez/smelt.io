1c45889e83eb69c2160aa26e5f3cb3e8291c64b6
Luke Segars <luke@theironyard.com>
2016-08-14 19:41:35 -0400

    Support for rendering challenge pages. More modular Sass structure.
    Also converted *.jade to *.pug, changes to Gulpfile to generate new pages, and
    a few other structural changes. Depends on the JSON schema changes from my last
    commit.

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,42 @@ let gulp = require('gulp');
 let sass = require('gulp-sass');
 let babel = require('gulp-babel');
 let pug = require('gulp-pug');
+let rename = require('gulp-rename');
+let fs = require('mz').fs;
 
-gulp.task('default', ['html', 'css', 'js']);
+function loadc(path) {
+    let challenges = [];
+
+    let c1 = JSON.parse(fs.readFileSync('challenges/palindrome.json').toString());
+    // c1.description.full = markdown(c1.description.full);
+    challenges.push(c1);
+
+    return challenges;
+}
+
+gulp.task('default', ['html', 'challenges', 'css', 'js']);
 
 gulp.task('html', function () {
-    return gulp.src('src/jade/*.jade')
-        .pipe(pug())
+    return gulp.src(['src/pug/challenges.pug', 'src/pug/index.pug'])
+        .pipe(pug({
+            locals: {
+                challenges: loadc('challenges/'),
+            },
+        }))
         .pipe(gulp.dest('public/'));
+});
+
+gulp.task('challenges', function () {
+    loadc('challenges/').forEach(challenge => {
+        return gulp.src('src/pug/challenge.pug')
+            .pipe(pug({
+                locals: {
+                    challenge: challenge,
+                },
+            }))
+            .pipe(rename(challenge.spec.func + '.html'))
+            .pipe(gulp.dest('public/challenges'));
+    });
 });
 
 gulp.task('css', function () {
@@ -30,8 +59,13 @@ gulp.task('js', function () {
 // TODO: add task for challenges.json generation
 // TODO: add task for challenges/*.js generation
 
-gulp.task('watch', ['html', 'css', 'js'], function () {
-    gulp.watch('src/*.html', ['html']);
+gulp.task('watch', ['html', 'challenges', 'css', 'js'], function () {
+    gulp.watch('src/pug/*.pug', ['html']);
+    gulp.watch('src/pug/partials/*.pug', ['html']);
     gulp.watch('src/js/*.js', ['js']);
     gulp.watch('src/scss/*.scss', ['css']);
+    gulp.watch('src/scss/partials/*.scss', ['css']);
+
+    gulp.watch('challenges/*', ['challenges']);
+    gulp.watch('src/pug/challenge*.pug', ['challenges']);
 });
