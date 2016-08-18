233f573a385e6a7ab18e8d01205d159dcb8bfda5
Luke Segars <luke@theironyard.com>
2016-08-18 19:30:11 -0400

    Improve deployment script to remove extraneous files.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,23 +5,32 @@
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Beginning deployment..."
 
+
+    # Remove files that don't need to be deployed'
+#    find public
+#    rm *
+#    rm -rf challenges/
+#    rm -rf node_modules/
+
+    git clone -b gh-pages https://anyweez:$GITHUB_ACCESS_TOKEN@github.com/anyweez/sorjs.com deploy
+
+#    git remote rm origin
+#    git remote add origin https://anyweez:$GITHUB_ACCESS_TOKEN@github.com/anyweez/sorjs.com
+#    git pull origin gh-pages
+#    git checkout gh-pages
+
+    # Copy in new content
+    cp -r public/* deploy/
+#    rm -rf public/
+
+#    find .
+    cd deploy
+
+    # Git configuration, commit, and push
     git config user.name "Travis CI"
     git config user.email "none@none.com"
-    
-    git remote rm origin
-    git remote add origin https://anyweez:$GITHUB_ACCESS_TOKEN@github.com/anyweez/sorjs.com
-    git pull origin gh-pages
-    git checkout gh-pages
-
-    find public
-    rm -rf challenges/
-    rm -rf node_modules/
-    cp -r public/* .
-    rm -rf public/
-
-    find .
     git add *
-    git push --set-upstream origin gh-pages
+#    git push --set-upstream origin gh-pages
     git commit -a -m "Automated push from Travis"
     git push
 
