c41d527b16384f71602965038b08589b450173be
Luke Segars <luke@theironyard.com>
2016-09-10 23:24:37 -0400

    Updated bucket name.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 # Deploy if this build is for the master branch. Otherwise no need to do any of this.
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Beginning deployment..."
-    aws s3 sync public s3://sorjs --acl public-read
+    aws s3 sync public s3://sorjs.com --acl public-read
 else
     echo "Skipping deployment (non-master branch $TRAVIS_BRANCH)."
 fi
