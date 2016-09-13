4a04f49ad2101465518337ace1acf56884bd195c
Luke Segars <luke@theironyard.com>
2016-09-13 18:08:36 -0400

    Jumpgame challenge.

---
+++ challenges/jumpgame.js
@@ -0,0 +1,18 @@
+const process = require('process');
+const test = require(process.env.SOR_RUNNER_DIR).test;
+const jumpgame = require('./sor.target');
+
+test('finds valid paths', t => {
+    t.true(jumpgame([1]));
+    t.true(jumpgame([5]));
+    t.true(jumpgame([1, 1, 2]));
+    t.true(jumpgame([2, 3, 1, 0, 2]));
+    t.true(jumpgame([2, 3, 1, 0, 3, 0, 1, 1]));
+    t.true(jumpgame([1, 2, 2, 3, 1, 0, 1]));
+});
+
+test('rejects when no valid paths exist', t => {
+    t.false(jumpgame([1, 1, 2, 0]));
+    t.false(jumpgame([2, 3, 1, 0, 3, 1, 8, 3, 4, 2, 3]));
+    t.false(jumpgame([1, 4, 2, 3, 1, 0, 1]));
+});
